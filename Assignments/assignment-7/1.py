50b7940cd9cab489a8688acfe1f9f9bb51130200
daniyalm97 <daniyalm97@gmail.com>
2018-07-27 23:57:01 +0530

    added files

---
+++ Assignments/assignment-7/1.py
@@ -0,0 +1,6 @@
+import math
+a=input('Enter 2 numbers:')
+b=input()
+print 'modulus:',a%b
+print 'exponent:',math.pow(a,b)
+print 'floor div:',math.floor(a/b)
---
+++ Assignments/assignment-7/3.py
@@ -0,0 +1,9 @@
+a=input('enter three numbers:')
+b=input()
+c=input()
+if a>b and a>c:
+   print a,'is greatest'
+elif b>c:
+   print b,'is greatest'
+else:
+   print c,'is greatest'
